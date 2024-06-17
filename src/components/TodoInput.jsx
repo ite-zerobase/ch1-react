65752e746309927397fd07c0bb22ab3fe7467b43
Seung-ryeol Choi <100273844+danielchoi1115@users.noreply.github.com>
2024-06-17 10:29:12 +0900

    Update comment in TodoInput.jsx

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -4,9 +4,11 @@ function TodoInput() {
   function addTodo() {
     /* 과제 2.3 addTodo 함수를 구현해보세요.
        addTodo는 사용자가 화면에 입력한 내용을 바탕으로 todo 객체를 만들어 todos를 업데이트하는 함수입니다.
+       todos에 더해질 todo객체는 다음과 같습니다.
        - todo 객체는 id, text, checked 속성을 가지고 있습니다.
-       - id는 todo 객체의 고유값으로 Date.now()로 생성합니다.
+       - id는 todo 객체의 고유값으로 "todo-" + Date.now()로 생성합니다.
        - checked의 기본값은 false 입니다.
+       - 새롭게 추가된 todo는 가장 위에 위치해야 합니다.
     */
   }
   return (
