4a3341d47ff5bbdd1bcd7bdcdf190bd322770f46
Choi Seung Ryeol <danielchoi1115@gmail.com>
2024-07-18 13:39:03 +0900

    Add bad example for useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import UseRefExample2 from './examples/UseRefExample2';
 import UseMemoExample from './examples/UseMemoExample';
 import UseCallbackExmaple from './examples/UseCallbackExmaple';
 import UseEffectExample from './examples/UseEffectExample';
+import UseEffectBadExample from './examples/UseEffectBadExample';
 
 function App() {
   return (
     <>
-      <UseRefExample1 />
+      {/* <UseRefExample1 /> */}
       {/* <UseRefExample2 /> */}
       {/* <UseMemoExample /> */}
       {/* <UseCallbackExmaple /> */}
       {/* <UseEffectExample /> */}
+      <UseEffectBadExample />
     </>
   );
 }
---
+++ src/examples/UseEffectBadExample.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+function UseEffectBadExample() {
+  const [count, setCount] = useState(1);
+  const array = ['one', 'two', 'three'];
+  const run = () => {
+    console.log('run');
+  };
+  const person = { age: 27, name: 승열 };
+
+  // useEffect(() => {
+  //   setCount((c) => c + 1);
+  // });
+
+  // useEffect(() => {
+  //   setCount((c) => c + 1);
+  // }, [array]);
+
+  // useEffect(() => {
+  //   setCount((c) => c + 1);
+  // }, [run]);
+
+  // useEffect(() => {
+  //   setCount((c) => c + 1);
+  // }, [person]);
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <button onClick={() => setCount((c) => c + 1)}>클릭</button>
+    </>
+  );
+}
+
+export default UseEffectBadExample;
